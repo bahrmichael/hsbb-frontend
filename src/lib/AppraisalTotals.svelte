fff07958304d94b0a5f7cdf0b7c8b2a80f61e18e
Michael Bahr <michael@bahr.dev>
2024-05-05 11:44:23 +0200

    fix: omit commas from total

--- src/lib/AppraisalTotals.svelte
+++ src/lib/AppraisalTotals.svelte
@@ -2,13 +2,17 @@
 	import copy from 'copy-to-clipboard';
 	import CopyToClipboardIcon from '$lib/icons/CopyToClipboard.svelte';
 	export let total: number;
+
+	// We round up so that we can copy a number without commas and dots.
+	// This caused issues with users who have a mix of English/German in their clients/OS.
+	$: value = Math.ceil(total);
 </script>
 
 <div class="flex justify-between">
 	<h2 class="card-title font-bold text-2xl">You get <span
-		class="text-success">{total.toLocaleString(undefined, {maximumFractionDigits: 2})}</span>ISK!</h2>
+		class="text-success">{value.toLocaleString()}</span>ISK!</h2>
 	<button class="btn btn-primary text-gray-100 justify-self-end"
-					on:click={() => copy(total.toFixed(2))}>
+					on:click={() => copy(value.toFixed())}>
 		<CopyToClipboardIcon />
 		Copy price
 	</button>
