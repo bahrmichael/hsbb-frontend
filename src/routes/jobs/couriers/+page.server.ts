01d95fe66bd866b689ba49a5cba416c8bd1eb079
Michael Bahr <michael@bahr.dev>
2024-06-18 04:30:10 +0200

    fix: load outstanding couriers from correct source

--- src/routes/jobs/couriers/+page.server.ts
+++ src/routes/jobs/couriers/+page.server.ts
@@ -1,22 +1,21 @@
 import { env } from '$env/dynamic/private';
-import { createClient } from '@vercel/kv';
 import { decodeJwt } from '$lib/decode-jwt.ts';
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-function getClient() {
-	return createClient({
-		url: env.KV_REST_API_URL,
-		token: env.KV_REST_API_TOKEN
-	});
-}
+const s3 = new S3Client({ region: 'us-east-1' });
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({cookies}) {
-	const data: any = await getClient().get('couriers-outstanding');
+	const body = await s3.send(new GetObjectCommand({
+		Bucket: env.AWS_BUCKET_NAME,
+		Key: 'couriers-outstanding',
+	})).then((res) => res.Body.transformToString());
+
 	const token = cookies.get('token-ingame');
 
 	return {
 		// the filter against d1 is a safety guard that this courier has received all the necessary data, and is not in a broken state
-		couriers: data.filter((c: any) => c.d1 > 0),
+		couriers: JSON.parse(body).filter((c: any) => c.d1 > 0),
 		authenticated: !!token,
 		characterName: token ? (await decodeJwt(token, 'ingame')).name : undefined,
 	}
