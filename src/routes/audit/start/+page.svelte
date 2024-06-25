e8ca4c68ae2384cc1cb5671aab3392e19ab0fd9c
Michael Bahr <michael@bahr.dev>
2024-06-25 13:18:57 +0200

    chore: extend audit scopes

--- src/routes/audit/start/+page.svelte
+++ src/routes/audit/start/+page.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div id="container" class="container mx-auto">
-	<a href={`https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=${encodeURIComponent(`https://highsec.evebuyback.com/callback`)}&client_id=ef1df42db7074451a575e32a8cc85f2c&state=audit&scope=${encodeURIComponent("esi-location.read_location.v1 esi-assets.read_assets.v1 esi-location.read_online.v1")}`} class="btn btn-primary">Click here to authenticate.</a>
+	<a href={`https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=${encodeURIComponent(`https://highsec.evebuyback.com/callback`)}&client_id=ef1df42db7074451a575e32a8cc85f2c&scope=${encodeURIComponent("esi-location.read_location.v1 esi-wallet.read_character_wallet.v1 esi-assets.read_assets.v1 esi-markets.read_character_orders.v1 esi-location.read_online.v1 esi-contracts.read_character_contracts.v1")}&state=audit`} class="btn btn-primary">Click here to authenticate.</a>
 </div>
