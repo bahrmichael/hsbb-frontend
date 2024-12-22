4bdb0bcbeba4eafcb529f3a1745b99a57c41729a
Michael Bahr <michael@bahr.dev>
2024-12-22 21:41:32 +0100

    store event

--- src/lib/EveStore.svelte
+++ src/lib/EveStore.svelte
@@ -5,7 +5,7 @@
 	export let selectedImage = '';
 	export let compact = false;
 
-	const plexSaleUntil = '2024-11-19';
+	const plexSaleUntil = '2024-12-25';
 	const isPlexSale = new Date() <= new Date(plexSaleUntil);
 	let showEveStore = isPlexSale;
 
@@ -23,7 +23,7 @@
 	onMount(async () => {
 		if (showEveStore) {
 			if (isPlexSale && !compact) {
-				selectedImage = '/store-event-plex-november.png';
+				selectedImage = '/store-event-plex-december.jpg';
 				campaign = 'plex';
 			} else if (new Date() <= new Date('2024-12-14') && !compact) {
 				selectedImage = '/store-event-skill-extractor.jpg';
