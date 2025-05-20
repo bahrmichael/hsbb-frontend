4f963bfdb0edd5879cae76a83d8a8c5aa9142244
Michael Bahr <michael@bahr.dev>
2025-05-20 21:21:14 +0200

    reformat appraisal

--- src/lib/Appraisal.svelte
+++ src/lib/Appraisal.svelte
@@ -31,7 +31,7 @@
 
 	let appraisalResult: AppraisalResult;
 
-	let calculatorInput: string = "";
+	let calculatorInput: string = '';
 	let isHsbb = true;
 
 	onMount(() => {
@@ -41,7 +41,7 @@
 			calculatorInput = rememberedText;
 			localStorage.removeItem('remember-text');
 		}
-	})
+	});
 
 	let isLoadingAppraisal = false;
 	let appraisalError: string | null;
@@ -49,13 +49,13 @@
 	async function submit() {
 		appraisalError = null;
 		if (!calculatorInput || calculatorInput.length < 3) {
-			appraisalError = "Enter at least 3 characters into the calculator";
-			return
+			appraisalError = 'Enter at least 3 characters into the calculator';
+			return;
 		}
 		const api = `https://5aa6btweli.execute-api.us-east-1.amazonaws.com/dev/appraise`;
 		isLoadingAppraisal = true;
 		try {
-			const payload: any = {subdomain: getInstance(), text: calculatorInput};
+			const payload: any = { subdomain: getInstance(), text: calculatorInput };
 			const referrer = localStorage.getItem('referrer');
 			if (referrer) {
 				payload.referrer = referrer;
@@ -66,13 +66,13 @@
 			const response = await fetch(api, {
 				method: 'post',
 				body: JSON.stringify(payload),
-				headers,
+				headers
 			});
 			const data = await response.json();
 			if (data.code === 'too_many_items') {
-				appraisalError = "Too many lines. Maximum 500 allowed.";
+				appraisalError = 'Too many lines. Maximum 500 allowed.';
 			} else if (data.code) {
-				appraisalError = "Unhandled error: " + data.code;
+				appraisalError = 'Unhandled error: ' + data.code;
 			} else {
 				appraisalResult = {
 					appraisalFailures: data.appraisalFailures,
@@ -83,16 +83,19 @@
 							typeId: item.typeId,
 							typeName: item.typeName,
 							quantity: item.quantity,
-							buybackPrice: item.buybackPriceForStack > 1_000 ? Math.round(item.buybackPriceForStack) : Math.round(item.buybackPriceForStack * 100) / 100,
+							buybackPrice:
+								item.buybackPriceForStack > 1_000
+									? Math.round(item.buybackPriceForStack)
+									: Math.round(item.buybackPriceForStack * 100) / 100,
 							effectiveRate: Math.ceil(item.buybackRate * 100),
 							pricingReasons: item.pricingReasons,
-							volumePerUnit: item.volumePerUnit,
+							volumePerUnit: item.volumePerUnit
 						};
-					}),
+					})
 				};
 			}
 		} catch (e) {
-			console.log({e});
+			console.log({ e });
 			appraisalError = 'Failed to appraise the items. Please try again in 5 minutes or later.';
 		} finally {
 			isLoadingAppraisal = false;
@@ -117,7 +120,9 @@
 			default:
 				throw Error('Unhandled instance. ClientId resolution.');
 		}
-		window.location.replace(`https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=${encodeURIComponent(`https://${instance}.evebuyback.com/callback`)}&client_id=${clientId}&state=nah`);
+		window.location.replace(
+			`https://login.eveonline.com/v2/oauth/authorize/?response_type=code&redirect_uri=${encodeURIComponent(`https://${instance}.evebuyback.com/callback`)}&client_id=${clientId}&state=nah`
+		);
 	}
 </script>
 
@@ -126,13 +131,19 @@
 		<AppraisalInfo />
 		<div class="card-body">
 			<span class="font-bold text-2xl">Appraise your items</span>
-			<textarea class="textarea textarea-bordered w-full" placeholder="Tritanium x10" rows="9"
-								bind:value={calculatorInput}></textarea>
+			<textarea
+				class="textarea textarea-bordered w-full"
+				placeholder="Tritanium x10"
+				rows="9"
+				bind:value={calculatorInput}
+			></textarea>
 			<div class="flex justify-between">
 				{#if appraisalResult == null || !!appraisalError}
 					<span class="text-sm text-base-content">Paste the items from your inventory above.</span>
 				{:else}
-					<span class="text-sm text-success">Appraisal succeeded. Scroll down to see the result.</span>
+					<span class="text-sm text-success"
+						>Appraisal succeeded. Scroll down to see the result.</span
+					>
 				{/if}
 			</div>
 			<div class="card-actions justify-end">
@@ -142,17 +153,26 @@
 						<span>{appraisalError}</span>
 					</div>
 				{/if}
-				<button class="btn" on:click={() => calculatorInput = ""}>Clear</button>
+				<button class="btn" on:click={() => (calculatorInput = '')}>Clear</button>
 				{#if !requiresSignIn}
-					<button class="btn btn-primary text-gray-100" on:click={submit} disabled={isLoadingAppraisal}>
+					<button
+						class="btn btn-primary text-gray-100"
+						on:click={submit}
+						disabled={isLoadingAppraisal}
+					>
 						{#if isLoadingAppraisal}
 							<span class="loading loading-spinner loading-sm"></span>
 						{/if}
 						Submit
 					</button>
 				{:else}
-					<div class="tooltip tooltip-info" data-tip="We require sign in to protect our server from malicious scripts.">
-						<button class="btn btn-primary text-gray-100" on:click={signin}>Sign in to enable the calculator</button>
+					<div
+						class="tooltip tooltip-info"
+						data-tip="We require sign in to protect our server from malicious scripts."
+					>
+						<button class="btn btn-primary text-gray-100" on:click={signin}
+							>Sign in to enable the calculator</button
+						>
 					</div>
 				{/if}
 			</div>
@@ -172,8 +192,10 @@
 				<div class="card bg-neutral shadow-xl">
 					<div class="card-body">
 						<AppraisalTotals total={appraisalResult.total} />
-						<AppraisalWarnings appraisalFailures={appraisalResult.appraisalFailures ?? ""}
-															 rejectionCodes={appraisalResult.rejectionCodes ?? []} />
+						<AppraisalWarnings
+							appraisalFailures={appraisalResult.appraisalFailures ?? ''}
+							rejectionCodes={appraisalResult.rejectionCodes ?? []}
+						/>
 						<AppraisalTable items={appraisalResult.items ?? []} />
 					</div>
 				</div>
