3bd1807fa3fb8349a173f8577c04df2cc8bf3431
Michael Bahr <michael@bahr.dev>
2025-05-20 21:19:17 +0200

    simplify EveStore

---
+++ src/routes/callback-new/+page.server.ts
@@ -0,0 +1,35 @@
+const HSBB = '7817dc406c90427db9d3570bb3cc495b';
+const LSBB = '542b75e8f71c4fb8aec7d58cdbc1fba5';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ cookies, request }) {
+	const url = new URL(request.url);
+	const searchParams = url.searchParams;
+	const clientId = searchParams.get('client_id');
+	const apiKey = searchParams.get('api_key');
+
+		// todo: handle things like audit that should be ignored, maybe?
+	const tokenName = (clientId === HSBB || clientId === LSBB) ? 'token-v1' : `token-${clientId}`;
+	cookies.set(tokenName, apiKey ?? '', {
+		path: '/',
+		maxAge: 60 * 60 * 24 * 365 * 10
+	});
+
+	if (clientId === 'waitlist') {
+		return {
+			redirectTo: '/jobs/waitlist'
+		};
+	} else if (clientId === 'ingame') {
+		return {
+			redirectTo: '/jobs/couriers'
+		};
+	} else if (clientId === 'audit') {
+		return {
+			redirectTo: '/audit/complete'
+		};
+	} else {
+		return {
+			redirectTo: '/'
+		};
+	}
+}
---
+++ src/routes/callback-new/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import ErrorIcon from '$lib/icons/Error.svelte';
+	import { page } from '$app/stores';
+
+	$: redirectTo = $page.data.redirectTo;
+	$: error= $page.data.error;
+
+	onMount(() => {
+		if (redirectTo) {
+			goto(redirectTo, { replaceState: true });
+		}
+	});
+</script>
+
+<div id="container" class="container mx-auto my-10">
+	{#if error}
+		<div role="alert" class="alert alert-error w-3/4">
+			<ErrorIcon />
+			<span>Failed to log in. Please go to the calculator and try again.</span>
+		</div>
+	{:else}
+		<progress class="progress w-24 mr-8"></progress>
+		<span class="text-xl text-bold">Logging in ...</span>
+	{/if}
+</div>
+
