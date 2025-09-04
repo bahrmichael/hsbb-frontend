b48105840e94f31f49dd388a70d375370bc87378
Michael Bahr <michael.bahr@sourcegraph.com>
2025-09-04 20:15:20 +0200

    remove banner

--- src/routes/appraisal/+page.svelte
+++ src/routes/appraisal/+page.svelte
@@ -167,11 +167,6 @@
 
 		<PlexBuybackBanner />
 		
-		<div role="alert" class="alert alert-info mb-10">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-			<span>We're building the fastest and most reliable appraisal for EVE Online. This tool is still in beta, and we welcome any feedback and bug reports!</span>
-		</div>
-
 		<a
 			href="/appraisal"
 			class="no-underline"
